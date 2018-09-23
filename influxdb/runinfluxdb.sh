accead2231e1279135dbdc515e4cb9a6c4024bc0
root <root@sqlmonitoring.xg4o2l0u0r4uxcnp5ymta4ceob.bx.internal.cloudapp.net>
2018-09-23 03:09:23 +0000

    change shells cript

--- influxdb/runinfluxdb.sh
+++ influxdb/runinfluxdb.sh
@@ -3,12 +3,12 @@
 # By default, this will run without modification, but if you want to change where the data directory gets mapped, you can do that here
 # Make sure this folder exists on the host.
 # This directory from the host gets passed through to the docker container.
-INFLUXDB_HOST_DIRECTORY="/mnt/influxdb"
+INFLUXDB_HOST_DIRECTORY="/influxdb"
 
 
-sudo docker run -d --net=host -v $INFLUXDB_HOST_DIRECTORY:/var/lib/influxdb:rw \
-	-p 8083:8083 \
+sudo docker run -d --net=host \
+	-v $INFLUXDB_HOST_DIRECTORY:/var/lib/influxdb:rw \
+	--hostname influxdb
  	-p 8086:8086 \
-	-p 25826:25826/udp \
         --name influxdb influxdb:latest
 
