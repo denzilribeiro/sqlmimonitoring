0e0bc8e0e0635a948bc603916b3694e6765aae83
root <root@testtelegraf.xg4o2l0u0r4uxcnp5ymta4ceob.bx.internal.cloudapp.net>
2018-09-24 21:20:36 +0000

    Docker container restart policy

--- influxdb/runinfluxdb.sh
+++ influxdb/runinfluxdb.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Make sure this folder exists on the host.Ideally it is a volume reserved for influxdb
 # This directory from the host gets passed through to the docker container.
 INFLUXDB_HOST_DIRECTORY="/influxdb"
@@ -8,6 +7,7 @@ INFLUXDB_HOST_DIRECTORY="/influxdb"
 sudo docker run -d --net=host \
 	-v $INFLUXDB_HOST_DIRECTORY:/var/lib/influxdb:rw \
 	--hostname influxdb \
+	--restart=always \
  	-p 8086:8086 \
         --name influxdb influxdb:latest
 
