9f801d767570c9a5e0d563f9db831f882ee89286
root <root@testtelegraf.xg4o2l0u0r4uxcnp5ymta4ceob.bx.internal.cloudapp.net>
2018-09-24 19:40:11 +0000

    committing influxdb change

--- influxdb/runinfluxdb.sh
+++ influxdb/runinfluxdb.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# By default, this will run without modification, but if you want to change where the data directory gets mapped, you can do that here
-# Make sure this folder exists on the host.
+# Make sure this folder exists on the host.Ideally it is a volume reserved for influxdb
 # This directory from the host gets passed through to the docker container.
 INFLUXDB_HOST_DIRECTORY="/influxdb"
 
 
 sudo docker run -d --net=host \
 	-v $INFLUXDB_HOST_DIRECTORY:/var/lib/influxdb:rw \
-	--hostname influxdb
+	--hostname influxdb \
  	-p 8086:8086 \
         --name influxdb influxdb:latest
 
